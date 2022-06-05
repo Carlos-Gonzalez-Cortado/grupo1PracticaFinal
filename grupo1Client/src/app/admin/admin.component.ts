ad4457e48e51cbb9b727a41b42cacc00f1472168
fjavier-fernandez <franciscojavier.fernandez@edu.upct.es>
2022-06-05 19:54:57 +0200

    Comienzo del componente Admin

--- grupo1Client/src/app/admin/admin.component.ts
+++ grupo1Client/src/app/admin/admin.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AdminComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    const body = document.querySelector('body'),
+      sidebar = body?.querySelector('nav'),
+      toggle = body?.querySelector(".toggle"),
+      searchBtn = body?.querySelector(".search-box"),
+      modeText = body?.querySelector(".mode-text");
+
+
+        toggle?.addEventListener("click" , () =>{
+            sidebar?.classList.toggle("close");
+        })
+
+        searchBtn?.addEventListener("click" , () =>{
+            sidebar?.classList.remove("close");
+        })
   }
 
 }
