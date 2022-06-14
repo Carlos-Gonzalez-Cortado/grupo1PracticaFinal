5d5c950c0b65f03c3b993cae5ae25947291e20dd
fjavier-fernandez <franciscojavier.fernandez@edu.upct.es>
2022-06-14 20:43:15 +0200

    Solución de deploy con Docker

--- grupo1Server/app/model/db.go
+++ grupo1Server/app/model/db.go
@@ -3,9 +3,6 @@ package model
 import (
 	"database/sql"
 	"fmt"
-	"log"
-
-	"github.com/joho/godotenv"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
@@ -14,14 +11,8 @@ import (
 var db *sql.DB
 
 func Init() {
-	err := godotenv.Load("config/.env")
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
-		return
-	}
-
-	db, err = sql.Open("mysql", "grupo1:M4ri4DB@tcp(192.168.195.254:52523)/grupo1")
+	db, err := sql.Open("mysql", "grupo1:M4ri4DB@tcp(192.168.195.254:52523)/grupo1")
 	if err != nil {
 		fmt.Printf("Error connecting to the DB: %s\n", err.Error())
 		return
